ba1651c8305bbe61c594c347046fb094e51d98cc
diogo jayme <diogojme@gmail.com>
2021-03-21 15:45:34 -0300

    Fix #3

--- app/src/main/java/app/storytel/candidate/com/view/posts_detail/PostDetailsFragment.kt
+++ app/src/main/java/app/storytel/candidate/com/view/posts_detail/PostDetailsFragment.kt
@@ -2,12 +2,26 @@ package app.storytel.candidate.com.view.posts_detail
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import app.storytel.candidate.com.R
+import app.storytel.candidate.com.model.Comment
+import app.storytel.candidate.com.shared.extensions.loadImageAsync
+import app.storytel.candidate.com.viewmodel.PostsViewModel
+import kotlinx.android.synthetic.main.fragment_post_details.*
+import org.koin.android.ext.android.inject
 
 class PostDetailsFragment : Fragment() {
+
+    private val postsViewModel: PostsViewModel by inject()
+    private val args by navArgs<PostDetailsFragmentArgs>()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +30,54 @@ class PostDetailsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_post_details, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBundleData()
+        setupToolbar()
+    }
+
+    private fun setupBundleData() {
+        val post = args.post
+        details.text = post?.title
+        backdrop.loadImageAsync(post?.photo?.url)
+        post?.id?.let { loadComments(it) }
+    }
+
+    private fun setupToolbar() {
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                findNavController().popBackStack(R.id.listPostsFragment, false)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun loadComments(postId: Int) {
+        postsViewModel.getComments(postId)
+
+        postsViewModel.loadingCommentsLiveData.observe(requireActivity(), Observer { isLoading ->
+            loading.visibility = if (isLoading) View.VISIBLE else View.GONE
+        })
+
+        postsViewModel.getCommentsLiveData.observe(requireActivity(), Observer {
+            setupData(it)
+        })
+    }
+
+    private fun setupData(comments: MutableList<Comment>) {
+        title1.text = comments[0].name
+        description1.text = comments[0].body
+        title2.text = comments[1].name
+        description2.text = comments[1].body
+        title3.text = comments[2].name
+        description3.text = comments[2].body
+
+    }
 }
 
