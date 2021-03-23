b16ccc0e1925890c41c11fdd7f9552e42b8a310f
diogo jayme <diogojme@gmail.com>
2021-03-22 22:38:59 -0300

    test ci

--- app/src/main/java/app/storytel/candidate/com/model/Comment.kt
+++ app/src/main/java/app/storytel/candidate/com/model/Comment.kt
@@ -4,6 +4,7 @@ import java.io.Serializable
 
 data class Comment(
     var postId: Int = 0,
+    var aaa: Int = 0,
     var id: Int? = 0,
     var name: String? = null,
     var email: String? = null,
